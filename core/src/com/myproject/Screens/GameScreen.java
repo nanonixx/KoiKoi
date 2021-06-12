82680587681bb6e7ff821cb7106601e174c878c6
Naomi Hidalgo <nhid@elpuig.xeill.net>
2021-06-12 02:51:21 +0200

    gameScreen template

--- core/src/com/myproject/Screens/GameScreen.java
+++ core/src/com/myproject/Screens/GameScreen.java
@@ -1,10 +1,35 @@
 package com.myproject.Screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.myproject.Config.BaseImageButton;
 import com.myproject.Config.BaseScreen;
 import com.myproject.MyGame;
 
 public class GameScreen extends BaseScreen {
+
+    private Texture background;
+    private BaseImageButton buttonBack;
+
     public GameScreen(MyGame game) {
         super(game);
+    }
+
+    @Override
+    public void show() {
+        background = new Texture("backgrounds/GameScreen.png");
+
+        buttonBack = new BaseImageButton("buttons/back.png", "buttons/back.png", 54, 54, 0, 666);
+        buttonBack.onClick(()-> setScreen(new MainMenuScreen(game)));
+        stage.addActor(buttonBack);
+    }
+
+    public void render(float delta) {
+        stage.getBatch().begin();
+        stage.getBatch().draw(background, 0, 0);
+        stage.getBatch().end();
+
+        stage.act(delta);
+        stage.draw();
     }
 }
